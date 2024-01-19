981443b30171fc118d559767e2b346a2159421c3
Daniel Popoola <daniel.popoola@ng.ab-inbev.com>
2024-01-19 12:22:13 +0100

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Deep learning project to go through the end to end lifecycle of a ML project using MLOps',
+    author='Daniel',
+    license='MIT',
+)
